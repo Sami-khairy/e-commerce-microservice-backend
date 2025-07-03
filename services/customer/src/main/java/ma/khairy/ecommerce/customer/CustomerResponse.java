d5d150f3773cfd842656be79a314770bcfcc6bba
Sami-Khairy <samikhairy2003@gmail.com>
2025-07-03 16:48:46 +0100

    create customer service

---
+++ services/customer/src/main/java/ma/khairy/ecommerce/customer/CustomerResponse.java
@@ -0,0 +1,11 @@
+package ma.khairy.ecommerce.customer;
+
+public record CustomerResponse(
+        String id,
+        String firstname,
+        String lastname,
+        String email,
+        Address address
+) {
+
+}
---
+++ services/customer/src/test/java/ma/khairy/ecommerce/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.khairy.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
